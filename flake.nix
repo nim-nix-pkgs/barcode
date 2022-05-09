021e3bd041a77caed218f879209a002de3ad2123
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:26 -0300

    chore: re index barcode

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."barcode-master".dir   = "master";
-  inputs."barcode-master".owner = "nim-nix-pkgs";
-  inputs."barcode-master".ref   = "master";
-  inputs."barcode-master".repo  = "barcode";
-  inputs."barcode-master".type  = "github";
-  inputs."barcode-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."barcode-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
